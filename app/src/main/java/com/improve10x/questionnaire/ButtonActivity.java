d80414b69646c3053041c3f2fc90a166bb2de942
CHARAN TEJA <charan.teja@improve10x.com>
2023-02-09 11:12:43 +0530

    Designed all xml files and wrote code for buttons activity

--- app/src/main/java/com/improve10x/questionnaire/ButtonActivity.java
+++ app/src/main/java/com/improve10x/questionnaire/ButtonActivity.java
@@ -2,13 +2,39 @@ package com.improve10x.questionnaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ButtonActivity extends AppCompatActivity {
+    Button radioButtonsBtn;
+    Button checkBoxesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        handleInitViews();
+        handleRadioButtonBtn();
+        handleCheckBoxBtn();
+    }
+
+    private void handleCheckBoxBtn() {
+        checkBoxesBtn.setOnClickListener(v -> {
+            Intent checkActivityIntent = new Intent(this,CeckBoxActivity.class);
+            startActivity(checkActivityIntent);
+        });
+    }
+
+    private void handleRadioButtonBtn() {
+        radioButtonsBtn.setOnClickListener(view -> {
+            Intent radioActivityIntent = new Intent(this,RadioButtonActivity.class);
+            startActivity(radioActivityIntent);
+        });
+    }
+
+    private void handleInitViews() {
+        radioButtonsBtn = findViewById(R.id.radio_button_btn);
+        checkBoxesBtn = findViewById(R.id.check_box_btn);
     }
 }
